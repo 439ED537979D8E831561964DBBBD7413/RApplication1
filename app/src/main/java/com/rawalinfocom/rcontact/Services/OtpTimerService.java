8817dfd6b25408f34930b597f9039b4d35fe9161
Monal <monal.godiwala@rawalinfocom.com>
2016-11-07 11:19:15 +0530

    added service for internal counter

--- app/src/main/java/com/rawalinfocom/rcontact/Services/OtpTimerService.java
+++ app/src/main/java/com/rawalinfocom/rcontact/Services/OtpTimerService.java
@@ -1,10 +1,189 @@
-package com.rawalinfocom.rcontact.Services;
+package com.rawalinfocom.rcontact.services;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.rawalinfocom.rcontact.asynctasks.AsyncWebServiceCall;
+import com.rawalinfocom.rcontact.constants.AppConstants;
+import com.rawalinfocom.rcontact.constants.WsConstants;
+import com.rawalinfocom.rcontact.database.DatabaseHandler;
+import com.rawalinfocom.rcontact.database.TableOtpLogDetails;
+import com.rawalinfocom.rcontact.enumerations.WSRequestType;
+import com.rawalinfocom.rcontact.helper.Utils;
+import com.rawalinfocom.rcontact.interfaces.WsResponseListener;
+import com.rawalinfocom.rcontact.model.OtpLog;
+import com.rawalinfocom.rcontact.model.WsRequestObject;
+import com.rawalinfocom.rcontact.model.WsResponseObject;
+
+import java.util.Date;
 
 /**
- * Created by user on 27/10/16.
+ * Created by Monal on 27/10/16.
+ * <p>
+ * Service to calculate 20 Minutes for Otp validation
  */
 
-public class OtpTimerService extends Service {
+public class OtpTimerService extends Service implements WsResponseListener {
+
+    private final static String LOG_TAG = "BroadcastService";
+
+    CountDownTimer cdt = null;
+
+    DatabaseHandler databaseHandler;
+
+    String mobileNumber;
+    long serviceEndTime;
+    boolean callMspServer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mobileNumber = intent.getStringExtra(AppConstants.EXTRA_MOBILE_NUMBER);
+        serviceEndTime = intent.getLongExtra(AppConstants.EXTRA_OTP_SERVICE_END_TIME, 20);
+        callMspServer = intent.getBooleanExtra(AppConstants.EXTRA_CALL_MSP_SERVER, true);
+
+        Log.e("onStartCommand: ", mobileNumber + " : " + serviceEndTime + " : " + callMspServer);
+
+        //        cdt = new CountDownTimer(Utils.OTP_VALIDITY_DURATION * 60 * 1000, 5 * 60 * 1000) {
+        cdt = new CountDownTimer(serviceEndTime, 10000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.e(LOG_TAG, "Countdown seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.e(LOG_TAG, "Timer finished");
+
+                databaseHandler = new DatabaseHandler(OtpTimerService.this);
+
+                TableOtpLogDetails tableOtpLogDetails = new TableOtpLogDetails(OtpTimerService
+                        .this, databaseHandler);
+                OtpLog otpLog = tableOtpLogDetails.getLastOtpDetails();
+
+                // Update OTP validity Flag to 0
+                otpLog.setOldValidityFlag("0");
+                tableOtpLogDetails.updateOtp(otpLog);
+
+                if (callMspServer) {
+                    // Get SMS status from third party
+                    getMspDeliveryStatus(otpLog);
+                }
+            }
+        };
+
+        cdt.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        cdt.cancel();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDeliveryResponse(String serviceType, Object data, Exception error) {
+        if (error == null) {
+
+            //<editor-fold desc="REQ_MSP_DELIVERY_TIME">
+            if (serviceType.equalsIgnoreCase(WsConstants.REQ_MSP_DELIVERY_TIME)) {
+                WsResponseObject mspDeliveryStatusResponse = (WsResponseObject) data;
+                if (mspDeliveryStatusResponse.getStatus().equalsIgnoreCase(WsConstants
+                        .RESPONSE_STATUS_TRUE)) {
+                    if (mspDeliveryStatusResponse.getOtpLog().getOldMspDeliveryTime() == null) {
+                        Log.e(LOG_TAG, "MSP Server not responding.");
+                    } else {
+
+                        databaseHandler = new DatabaseHandler(OtpTimerService.this);
+
+                        TableOtpLogDetails tableOtpLogDetails = new TableOtpLogDetails
+                                (OtpTimerService.this, databaseHandler);
+                        OtpLog otpLog = tableOtpLogDetails.getLastOtpDetails();
+
+                        Date currentValidityTime = Utils.getStringDateToDate(otpLog
+                                .getOldValidUpto());
+                        Date mspValidityTime = Utils.getStringDateToDate(Utils.getOtpExpirationTime
+                                (mspDeliveryStatusResponse.getOtpLog().getOldMspDeliveryTime()));
+
+                        Log.e("currentValidityTime: ", currentValidityTime.toString() + " ");
+                        Log.e("mspValidityTime: ", mspValidityTime + " ");
+                        Log.e("difference: ", mspValidityTime.getTime() -
+                                currentValidityTime.getTime() + " ");
+
+                        try {
+                            if (mspValidityTime.after(currentValidityTime)) {
+                                otpLog.setOldValidityFlag("1");
+                                otpLog.setOldValidUpto(Utils.getOtpExpirationTime
+                                        (mspDeliveryStatusResponse.getOtpLog()
+                                                .getOldMspDeliveryTime()));
+                                otpLog.setOldMspDeliveryTime(mspDeliveryStatusResponse.getOtpLog()
+                                        .getOldMspDeliveryTime());
+                                tableOtpLogDetails.updateOtp(otpLog);
+
+                                Intent otpServiceIntent = new Intent(this, OtpTimerService.class);
+                                otpServiceIntent.putExtra(AppConstants.EXTRA_MOBILE_NUMBER,
+                                        mobileNumber);
+                                otpServiceIntent.putExtra(AppConstants
+                                        .EXTRA_OTP_SERVICE_END_TIME, mspValidityTime.getTime() -
+                                        currentValidityTime.getTime());
+                                otpServiceIntent.putExtra(AppConstants.EXTRA_CALL_MSP_SERVER,
+                                        false);
+                                startService(otpServiceIntent);
+                            } else {
+                                otpLog.setOldValidityFlag("0");
+                                tableOtpLogDetails.updateOtp(otpLog);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            // Update OTP validity Flag to 0
+                            otpLog.setOldValidityFlag("0");
+                            tableOtpLogDetails.updateOtp(otpLog);
+                        }
+
+                        /*Toast.makeText(this, mspDeliveryStatusResponse.getOtpLog()
+                                .getOldMspDeliveryTime(), Toast.LENGTH_SHORT).show();*/
+                    }
+                } else {
+                    Log.e("error response", mspDeliveryStatusResponse.getMessage());
+                }
+            }
+            //</editor-fold>
+
+        } else {
+            Log.e("onDeliveryResponse: ", error.getLocalizedMessage());
+        }
+    }
+
+    private void getMspDeliveryStatus(OtpLog otpLog) {
+
+        WsRequestObject mspDeliveryStatusObject = new WsRequestObject();
+        mspDeliveryStatusObject.setPmId(otpLog.getRcProfileMasterPmId());
+        mspDeliveryStatusObject.setMobileNumber(mobileNumber);
+        mspDeliveryStatusObject.setOtp(otpLog.getOldOtp());
+        mspDeliveryStatusObject.setOtpGenerationTime(otpLog.getOldGeneratedAt());
+
+        if (Utils.isNetworkAvailable(this)) {
+            new AsyncWebServiceCall(this, WSRequestType.REQUEST_TYPE_JSON.getValue(),
+                    mspDeliveryStatusObject, null, WsResponseObject.class, WsConstants
+                    .REQ_MSP_DELIVERY_TIME, null).execute(WsConstants.WS_ROOT + WsConstants
+                    .REQ_MSP_DELIVERY_TIME);
+        }
+    }
 }
