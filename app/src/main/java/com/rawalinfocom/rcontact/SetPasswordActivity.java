7f3e282c5b1b0cd21e4c43a0613526b150c0ab95
Maulik Dhameliya <maulik.dhameliya@rawalinfocom.com>
2017-06-14 15:46:52 +0530

    Enter Password Screen Design

--- app/src/main/java/com/rawalinfocom/rcontact/SetPasswordActivity.java
+++ app/src/main/java/com/rawalinfocom/rcontact/SetPasswordActivity.java
@@ -12,6 +12,8 @@ import com.rawalinfocom.rcontact.helper.RippleView;
 import com.rawalinfocom.rcontact.helper.Utils;
 import com.rawalinfocom.rcontact.interfaces.WsResponseListener;
 
+import org.apache.commons.lang3.StringUtils;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -58,6 +60,7 @@ public class SetPasswordActivity extends BaseActivity implements RippleView
 
     private void init() {
         rippleActionBack.setOnRippleCompleteListener(this);
+        rippleRegister.setOnRippleCompleteListener(this);
         textToolbarTitle.setText(getResources().getString(R.string.set_password));
         textToolbarTitle.setTypeface(Utils.typefaceRegular(this));
         textConfigurePassword.setTypeface(Utils.typefaceRegular(this));
@@ -68,6 +71,11 @@ public class SetPasswordActivity extends BaseActivity implements RippleView
         textTip.setTypeface(Utils.typefaceRegular(this));
     }
 
+    private boolean isPasswordValid(String password) {
+        String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return password.matches(pattern);
+    }
+
     @Override
     public void onDeliveryResponse(String serviceType, Object data, Exception error) {
 
@@ -75,6 +83,38 @@ public class SetPasswordActivity extends BaseActivity implements RippleView
 
     @Override
     public void onComplete(RippleView rippleView) {
+        switch (rippleView.getId()) {
+            case R.id.ripple_action_back:
+                onBackPressed();
+                break;
+            case R.id.ripple_register:
+                registerButtonClicked();
+                break;
+        }
+    }
+
+    private void registerButtonClicked() {
+        String password = inputSetPassword.getText().toString();
+        String confirmPassword = inputSetConfirmPassword.getText().toString();
+        if (StringUtils.isEmpty(password)) {
+            Utils.showErrorSnackBar(this, layoutRoot, getResources().getString(R.string.err_msg_please_enter_password));
+            return;
+        }
+        if (StringUtils.isEmpty(confirmPassword)) {
+            Utils.showErrorSnackBar(this, layoutRoot, getResources().getString(R.string.err_msg_please_confirm_password));
+            return;
+        }
+        if (!password.equalsIgnoreCase(confirmPassword)) {
+            Utils.showErrorSnackBar(this, layoutRoot, getResources().getString(R.string.err_msg_password_not_match));
+            return;
+        }
+        if (password.equalsIgnoreCase(confirmPassword)) {
+            if (isPasswordValid(password)) {
+                Utils.showErrorSnackBar(this, layoutRoot, getResources().getString(R.string.msg_please_wait));
+            }else {
+                Utils.showErrorSnackBar(this,layoutRoot,getResources().getString(R.string.msg_tip_password));
+            }
+        }
 
     }
 }
