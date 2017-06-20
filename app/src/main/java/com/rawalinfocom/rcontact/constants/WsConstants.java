b6eb5db2ced7f802f62a7626f2016c3365bbd6bf
Maulik Dhameliya <maulik.dhameliya@rawalinfocom.com>
2017-06-20 15:48:21 +0530

    Move Syncing logic from fragment to service

--- app/src/main/java/com/rawalinfocom/rcontact/constants/WsConstants.java
+++ app/src/main/java/com/rawalinfocom/rcontact/constants/WsConstants.java
@@ -15,14 +15,14 @@ public class WsConstants {
 
     // QA Server
 //    public static final String WS_ROOT = "http://54.186.222.230/api/v1/";
-    public static final String WS_ROOT = "http://apiqa.rcontacts.in/api/v1/";
+//    public static final String WS_ROOT = "http://apiqa.rcontacts.in/api/v1/";
 
     // Aniruddh
 //    public static final String WS_ROOT = "http://10.0.21.122/api/v1/";
     //  public static final String WS_ROOT = "http://10.0.21.122/api/v1/";
 
     //    Maulik
-//    public static final String WS_ROOT = "http://10.0.51.119/api/v1/";
+    public static final String WS_ROOT = "http://10.0.51.119/api/v1/";
 
     // Shailesh
 //    public static final String WS_ROOT = "http://10.0.21.14:8001/api/v1/";
