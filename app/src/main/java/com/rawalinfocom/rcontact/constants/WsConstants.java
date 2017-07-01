4704a820a4977c8158a9440d4a77a9a4f41926a6
Aniruddh <aniruddh.pal@rawalinfocom.com>
2017-07-01 12:32:46 +0530

    - Staging api change

--- app/src/main/java/com/rawalinfocom/rcontact/constants/WsConstants.java
+++ app/src/main/java/com/rawalinfocom/rcontact/constants/WsConstants.java
@@ -9,16 +9,18 @@ package com.rawalinfocom.rcontact.constants;
 public class WsConstants {
 
     // Staging Env
-//    public static final String WS_ROOT = "http://apistaging.rcontacts.in";
+//    public static final String WS_ROOT = "http://apistaging.rcontacts.in/api/v1/";
+
+//    public static final String WS_ROOT = "http://appstaging.rcontacts.in/api/v1/";
 
     // QA Server
-    public static final String WS_ROOT = "http://apiqa.rcontacts.in/api/v1/";
+//    public static final String WS_ROOT = "http://apiqa.rcontacts.in/api/v1/";
 
     // Monal
 //    public static final String WS_ROOT = "http://10.0.21.163/api/v1/";
 
     // Aniruddh
-//    public static final String WS_ROOT = "http://10.0.21.122/api/v1/";
+    public static final String WS_ROOT = "http://10.0.21.122/api/v1/";
 
     //    Maulik
 //    public static final String WS_ROOT = "http://10.0.51.119/api/v1/";
@@ -59,6 +61,8 @@ public class WsConstants {
     // Staging Env
 //    public static final String WS_PROFILE_VIEW_ROOT = "http://webstaging.rcontacts.in/global/public/";
 
+    // QA Server
+//    public static final String WS_PROFILE_VIEW_ROOT = "http://webqa.rcontacts.in/global/public/";
 
     public static final String RESPONSE_STATUS_TRUE = "true";
 
