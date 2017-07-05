aee382aeb08e17609acb3bdbe077eda4d6afa0c6
Maulik Dhameliya <maulik.dhameliya@rawalinfocom.com>
2017-07-05 15:13:28 +0530

    1. removed formatted number in syncing 2. remove comments from call fragment

--- app/src/main/java/com/rawalinfocom/rcontact/contacts/AllContactsListFragment.java
+++ app/src/main/java/com/rawalinfocom/rcontact/contacts/AllContactsListFragment.java
@@ -1278,9 +1278,8 @@ public class AllContactsListFragment extends BaseFragment implements LoaderManag
                             ProfileDataOperationPhoneNumber phoneNumber = new
                                     ProfileDataOperationPhoneNumber();
 
-                            phoneNumber.setPhoneNumber(Utils.getFormattedNumber(getActivity(), cursor
-                                    .getString(cursor.getColumnIndex(ContactsContract
-                                            .CommonDataKinds.Phone.NUMBER))));
+                            phoneNumber.setPhoneNumber(cursor.getString(cursor.getColumnIndex(ContactsContract
+                                    .CommonDataKinds.Phone.NUMBER)));
                             phoneNumber.setPhoneType(phoneBookContacts.getPhoneNumberType
                                     (cursor.getInt(cursor.getColumnIndex
                                             (ContactsContract.CommonDataKinds.Phone.TYPE))));
