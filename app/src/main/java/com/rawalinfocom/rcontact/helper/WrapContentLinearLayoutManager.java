e4e5c28317fb2872bb788301553fca5049d25e57
Hardik <hardik.dungrani@rawalinfocom.com>
2017-07-06 18:28:32 +0530

    -- Code optimization in Call log page. -- set +91 number format while place a call.

--- app/src/main/java/com/rawalinfocom/rcontact/helper/WrapContentLinearLayoutManager.java
+++ app/src/main/java/com/rawalinfocom/rcontact/helper/WrapContentLinearLayoutManager.java
@@ -13,16 +13,8 @@ import android.util.Log;
 public class WrapContentLinearLayoutManager extends LinearLayoutManager {
 
 
-    public WrapContentLinearLayoutManager(Context context) {
-        super(context);
-    }
-
     public WrapContentLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
         super(context, orientation, reverseLayout);
-    }
-
-    public WrapContentLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
