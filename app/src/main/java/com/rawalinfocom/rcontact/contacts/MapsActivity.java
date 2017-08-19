b482c07076125e8182b049909aaca9960e4d16ef
Monal <monal.godiwala@rawalinfocom.com>
2017-08-19 10:48:55 +0530

    - Added "my location" in maps screen

--- app/src/main/java/com/rawalinfocom/rcontact/contacts/MapsActivity.java
+++ app/src/main/java/com/rawalinfocom/rcontact/contacts/MapsActivity.java
@@ -1,9 +1,13 @@
 package com.rawalinfocom.rcontact.contacts;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -130,16 +134,17 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback, Ri
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         this.googleMap = googleMap;
-/*        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.setMyLocationEnabled(true);*/
-  /*      if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
-                    .ACCESS_FINE_LOCATION}, AppConstants
-                    .MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        } else {*/
         googleMap.getUiSettings().setZoomControlsEnabled(true);
-//        googleMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
+                    .ACCESS_FINE_LOCATION, Manifest.permission
+                    .ACCESS_COARSE_LOCATION}, AppConstants
+                    .MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            googleMap.setMyLocationEnabled(true);
+        }
        /* Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -365,7 +370,7 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback, Ri
         }
     }
 
-    /* @Override
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -373,24 +378,26 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback, Ri
             case AppConstants.MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED) {
 
                     // Permission Granted
 
                     if (Utils.isLocationEnabled(this)) {
                         googleMap.getUiSettings().setZoomControlsEnabled(true);
                         googleMap.setMyLocationEnabled(true);
-                        gpsTracker = new GPSTracker(this, null);
+                      /*  gpsTracker = new GPSTracker(this, null);
                         latitude = gpsTracker.getLatitude();
                         longitude = gpsTracker.getLongitude();
 
-                        addMapMarker();
-                    } else {
-                        gpsTracker.showSettingsAlert();
+                        addMapMarker();*/
                     }
+                  /*  else {
+                        gpsTracker.showSettingsAlert();
+                    }*/
 
-
-                } else {
+                }
+                /*else {
 
                     // Permission Denied
                     latitude = 21.1702;
@@ -398,11 +405,11 @@ public class MapsActivity extends BaseActivity implements OnMapReadyCallback, Ri
                     addMapMarker();
 
 
-                }
+                }*/
             }
             break;
         }
-    }*/
+    }
 
     //</editor-fold>
 
