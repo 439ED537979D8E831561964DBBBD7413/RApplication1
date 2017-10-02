cea643827c2a2308d255d330cf30a201c698b51b
Maulik Dhameliya <maulik.dhameliya@rawalinfocom.com>
2017-10-02 13:30:51 +0530

    master : Solved issues raised by QA team.

--- app/src/main/java/com/rawalinfocom/rcontact/database/DatabaseHandler.java
+++ app/src/main/java/com/rawalinfocom/rcontact/database/DatabaseHandler.java
@@ -14,7 +14,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     // All Static variables
     // Database Version
-    private static final int DATABASE_VERSION = 4;  //update to 4 for playstore // For QA/Staging 5
+    private static final int DATABASE_VERSION = 5;  //update to 4 for production/staging // For QA 5
 
     // Database Name
     public static final String DATABASE_NAME = "RContact.db";
@@ -102,6 +102,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                 // For Email
                 db.execSQL("ALTER TABLE " + TableEmailMaster.TABLE_RC_EMAIL_MASTER + " ADD "
                         + "COLUMN " + TableEmailMaster.COLUMN_EM_SOCIAL_TYPE + " text ");
+
+
+                /*System.out.println("RContact db upgrade case 3 --> ");
+                // For version 4
+                db.execSQL(TableEmailMaster.CREATE_TABLE_RC_EMAIL_MASTER_1);
+                db.execSQL("INSERT INTO " + TableEmailMaster.TABLE_RC_EMAIL_MASTER_TEMP + " SELECT * FROM " +
+                        TableEmailMaster.TABLE_RC_EMAIL_MASTER + ";");
+                db.execSQL("DROP TABLE IF EXISTS '" + TableEmailMaster.TABLE_RC_EMAIL_MASTER + "'");
+                db.execSQL("ALTER TABLE " + TableEmailMaster.TABLE_RC_EMAIL_MASTER_TEMP
+                        + " RENAME TO " + TableEmailMaster.TABLE_RC_EMAIL_MASTER + ";");*/
 
             case 4:
                 System.out.println("RContact db upgrade case 4 --> ");
