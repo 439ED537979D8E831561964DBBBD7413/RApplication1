56ce93c4b6fa7d582ede869d39e5907685c7c53a
Aniruddh <aniruddh.pal@rawalinfocom.com>
2017-12-13 10:24:58 +0530

    Master: - Maintaining database version

--- app/src/main/java/com/rawalinfocom/rcontact/database/DatabaseHandler.java
+++ app/src/main/java/com/rawalinfocom/rcontact/database/DatabaseHandler.java
@@ -15,7 +15,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     // All Static variables
     // Database Version
     private static final int DATABASE_VERSION = 7;  //update to 7 for production/staging/QA
-                                                    // previous version 7
+                                                    // previous version 6
 
     // Database Name
     public static final String DATABASE_NAME = "RContact.db";
